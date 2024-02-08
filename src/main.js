9647b64091960f0f5d5eedc9372058c1f0e12795
mir <mir@asaf.com>
2024-02-08 21:09:30 +0300

    Updates

--- src/main.js
+++ src/main.js
@@ -32,4 +32,5 @@ import '../public/js/jquery.nice-select.min.js'
 import '../public/js/jquery-ui.min.js'
 import '../public/js/api.js'
 import '../public/js/main.js'
+import '../public/js/SoccerJersey.js'
 
